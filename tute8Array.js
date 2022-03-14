547bc5073c4f34b5a286e165d8fe08f71d26ae46
iampradeep <91717371+pradeep66927@users.noreply.github.com>
2022-03-14 16:23:03 +0530

    initial commit
    code with harry tute8 of 66

---
+++ tute8Array.js
@@ -0,0 +1,157 @@
+// console.log("this is tute 8 we have to learn about if else")
+
+// const age = 19;
+// if (age==19) {
+//     console.log('you are eligble for vote')
+// } else if (age==45) {
+//     console.log("age is enough for vote ")
+// }
+//  else {
+//     console.log('you are not eligble for vote')
+// }
+
+
+// if (typeof vari!=="undefined") {
+//     console.log("vari is defined")
+// }
+// else{
+//     console.log("vari is not defined")
+// }
+
+// ===  it check value and type both
+// means "18" value=18 , type=string
+
+//let arr=[2,3,4,5,6,"raj",7,8,9]
+//console.table(arr) //it gives index and element in table
+
+/*******and(&&) or  or(||) variable******************** */
+
+// const doesDrive = true;
+// if (doesDrive && age >18) {
+//     console.log("you can drive");
+// }
+// else {
+//     console.log("you can't drive");
+// }
+
+/******* or(||) oprator ******************** */
+
+// const doesDrive = false;
+// if (doesDrive || age >18) {
+//     console.log("you can drive");
+// }
+// else {
+//     console.log("you can't drive");
+// }
+
+/********* ternary operator ************************** */
+
+// const doesDrive = false;
+// console.log(age==48? 'age is 48':'age is not 48');
+
+/*******************       Switch case *********** */
+
+
+
+/***************************************************** */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//qn-1
+//answer:-java script is text based programming language for the 
+//client side and server side both it help to make web page or
+// webside attractice.
+
+//Qn-2
+//answer:-var is container which have function scope , we use it to define and further we do operation on it . we can reasign it 
+//       let is also a container have block scope ,we use it to define. it also can be reasign.
+//       const is also a container but we can't reasign it.
+
+//Qn-3
+// ans:-a.Number(primitive data type)
+// ans:-b.reference data type
+
+//Qn-4  *************************************
+//a.1
+let arr=[];
+//a.ii
+arr.push("amit");
+arr.push("vikash")
+arr.push("mahendra")
+console.log(arr);
+// a.iii ***************************************
+//
+
+
+//a.iv *******************************************
+arr.pop()
+console.log(arr)
+
+// b.object ****************************************
+//b.i.
+var obj={
+
+};
+
+// b.ii *********************************************
+obj["Name"] = "pradeep";
+obj['LAST NAME'] = "kumar";
+obj["age"] = 20
+obj["Native Place"] = "dharamshala"
+obj["Agenda"] = "javascript"
+//console.log(obj)
+
+// b. iii. ***************************
+var objother ={
+    
+};
+objother[obj]=""
+
+//console.log(objother)
+
+// 5. mrrging to array ******************************
+myArr = ["pradeep","j.s",]
+
+arr = arr.concat(myArr);
+console.log(arr)
+
+// 6. ************************************************
+
+// b. ternary operator
+// ans:-advantage :-it makes the code in less line 
+// ans:- disadvantage :- 
+
+// 7. 
+// 8.
+
+// 9.
+var arr1 = [1,2,3,4,5]
+var arr2 = [4,5]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
